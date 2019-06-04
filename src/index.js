2b632b140d60489103c3ec8635a9b6702652960b
Indirachsm <indyrachsm@gmail.com>
2019-06-04 12:15:15 -0500

    estilos y login

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-
-const contrasena = getElementById('validar').value;
-const btn1 = getElementById('btn1');
+const contrasena = document.getElementById("contrasena");
+const btn1 = document.getElementById("btn1");
+const respuestaCorrecta = "LABORATORIA"
 
 btn1.addEventListener('click', () => {
-  if (contrasena == "LABORATORIA")
+  if (contrasena.value == respuestaCorrecta)
   {
-  document.getElementById('div1').style.display = "none"
-  document.getElementById('div2').style.display = "block"
+    document.getElementById('div1').style.display = "none"
+    document.getElementById('div3').style.display = "none"
+    document.getElementById('div2').style.display = "block"
 }
   else {
-  document.getElementById('div1').style.display = "none"
-  document.getElementById('div3').style.display = "block"
+    document.getElementById('div1').style.display = "none"
+    document.getElementById('div2').style.display = "none"
+    document.getElementById('div3').style.display = "block"
 }});
