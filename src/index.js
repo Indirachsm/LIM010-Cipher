f310cc0c4ddd74bf12950da49c048598fa18d9dc
Indirachsm <indyrachsm@gmail.com>
2019-06-06 11:58:06 -0500

    LOGIN Y ERROR COMPLETO

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const contrasena = document.getElementById("contrasena");
 const btn1 = document.getElementById("btn1");
 const respuestaCorrecta = "LABORATORIA"
+let contador = 0;
 
 btn1.addEventListener('click', () => {
   if (contrasena.value == respuestaCorrecta)
@@ -10,7 +11,16 @@ btn1.addEventListener('click', () => {
     document.getElementById('div2').style.display = "block"
 }
   else {
-    document.getElementById('div1').style.display = "none"
+    document.getElementById('div1').style.display = "block"
     document.getElementById('div2').style.display = "none"
-    document.getElementById('div3').style.display = "block"
-}});
+    document.getElementById('div3').style.display = "none"
+    document.getElementById('mensjPasswordIncorrecta').innerHTML="Contraseña incorrecta, inténtalo nuevamente"
+    contador ++
+    if(contador===3){
+      document.getElementById('div1').style.display = "none"
+      document.getElementById('div2').style.display = "none"
+      document.getElementById('div3').style.display = "none"
+      document.getElementById('div4').style.display = "block"
+    }
+}
+})
